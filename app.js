808a7059e2622031966a2633947f95fa7ac754b6
Mehmet Latif <mehmetlatif0101@gmail.com>
2022-04-09 00:23:50 +0300

    mongo connect app taşındı.

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const bodyParser = require("body-parser");
 //const mongoose = require("mongoose");
 require("dotenv").config();
 const PORT = process.env.PORT || 4040;
-require("./data/db-config");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("Db connection successful !")
+);
+
 
 const persons = require("./routers/persons");
 app.use("/persons", persons);
